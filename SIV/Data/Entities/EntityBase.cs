12e0ba0961af67575dabe77d640e0a4bead8d153
JohanGodinez <johang77@hotmail.com>
2020-05-16 11:49:47 -0600

    First Commit, Created and configure project

---
+++ SIV/Data/Entities/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Entities
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+        public Guid RowCode { get; set; }
+        public int UserCreated { get; set; }
+    }
+}
