694eb2b8b3487fde94f68a72578e466219519f7c
Félix Javier Ramírez <felixramirez19892015@gmail.com>
2020-05-16 22:04:02 -0600

    create BaseRepository IBaseRepository

--- SIV/Repository/Interface/IBaseRepository.cs
+++ SIV/Repository/Interface/IBaseRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -7,6 +9,35 @@ namespace Repository.Interface
 {
     public interface IBaseRepository<TModel> where TModel : class
     {
+        /// <summary>
+        /// Method create entity BaseRepository
+        /// </summary>
+        /// <param name="entity">Entity Data Base</param>
+        /// <returns><see cref="TModel"/></returns>
         TModel Create(TModel entity);
+        /// <summary>
+        /// Method update entity
+        /// </summary>
+        /// <param name="entity">Entity Data Base</param>
+        /// <returns><see cref="TModel"/></returns>
+        TModel Update(TModel entity);
+        /// <summary>
+        /// Method delete entity
+        /// </summary>
+        /// <param name="entity">Entity Data Base</param>
+        /// <returns><see cref="TModel"/></returns>
+        TModel Delete(TModel entity);
+        /// <summary>
+        /// Method filter entity in database
+        /// </summary>
+        /// <param name="expression">Expression filter</param>
+        /// <returns>List <see cref="TModel"/></returns>
+        IQueryable<TModel> FindBy(Expression<Func<TModel, bool>> expression);
+        /// <summary>
+        /// Method filter entity single
+        /// </summary>
+        /// <param name="id">Id unique entity</param>
+        /// <returns><see cref="TModel"/></returns>
+        TModel FindById(int id);
     }
 }
--- SIV/Repository/Repository/BaseRepository.cs
+++ SIV/Repository/Repository/BaseRepository.cs
@@ -4,6 +4,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Data;
 using Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace Repository.Repository
 {
@@ -19,14 +22,55 @@ namespace Repository.Repository
         /// <summary>
         /// Method create entity BaseRepository
         /// </summary>
-        /// <param name="entity">ModelDb to insert</param>
-        /// <returns></returns>
+        /// <param name="entity">Entity Data Base</param>
+        /// <returns><see cref="TModel"/></returns>
         public TModel Create(TModel entity)
         {
-            db.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+            db.Entry(entity).State = EntityState.Added;
             db.SaveChanges();
 
             return entity;
+        }
+        /// <summary>
+        /// Method update entity
+        /// </summary>
+        /// <param name="entity">Entity Data Base</param>
+        /// <returns><see cref="TModel"/></returns>
+        public TModel Update(TModel entity)
+        {
+            db.Entry(entity).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return entity;
+        }
+        /// <summary>
+        /// Method delete entity
+        /// </summary>
+        /// <param name="entity">Entity Data Base</param>
+        /// <returns><see cref="TModel"/></returns>
+        public TModel Delete(TModel entity)
+        {
+            db.Entry(entity).State = EntityState.Deleted;
+            db.SaveChanges();
+            return entity;
+        }
+        /// <summary>
+        /// Method filter entity in database
+        /// </summary>
+        /// <param name="expression">Expression filter</param>
+        /// <returns>List <see cref="TModel"/></returns>
+        public IQueryable<TModel> FindBy(Expression<Func<TModel, bool>> expression)
+        {
+            return db.Set<TModel>().Where(expression);
+        }
+        /// <summary>
+        /// Method filter entity single
+        /// </summary>
+        /// <param name="id">Id unique entity</param>
+        /// <returns><see cref="TModel"/></returns>
+        public TModel FindById(int id)
+        {
+            return db.Set<TModel>().Find(id);
         }
     }
 }
